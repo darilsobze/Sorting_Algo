620cd843ea2176ff26932e4f16a284d224061e6f
darilsobze <darilsobze13@icloud.com>
2024-06-19 11:17:56 +0200

    SelectionSort

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main( String [] args ){
+        System.out.println("Hello world!");
+        System.out.println(Arrays.toString(selectionSort(new int[]{30, 12, 20, 9, 15})));
+
+    }
+
+
+    private static int[] selectionSort ( int[] tab){
+        for (int i=0; i<tab.length-1 ; i++){
+            int iMin = i;
+            for (int j=i+1; j<= tab.length-1 ; j++){
+                if (tab[j]< tab[iMin])
+                    iMin=j; // the number with index j becomes the minimum
+            }
+            // swap the both numbers with index iMin and i
+            int temp= tab[iMin];
+            tab[iMin]= tab[i];
+            tab[i]= temp;
+        }
+        return tab;
+    }
+    
+}
