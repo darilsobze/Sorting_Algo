b072b2020086029d1e48980512d36a9ed466593c
darilsobze <darilsobze13@icloud.com>
2024-06-19 11:33:01 +0200

    InsertionSort

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main( String [] args ){
         System.out.println("Hello world!");
         System.out.println(Arrays.toString(selectionSort(new int[]{30, 12, 20, 9, 15})));
+        System.out.println(Arrays.toString(insertionSort(new int[]{30, 12, 20, 9, 15})));
 
     }
 
@@ -22,5 +23,16 @@ public class Main {
         }
         return tab;
     }
-    
+    private static int[] insertionSort( int[] tab){
+        for (int i=1; i<= tab.length-1; i++){
+            int key = tab[i];
+            int j=i-1;
+            while (j>=0 && tab[j] > key){
+                tab[j+1] = tab[j];
+                j=j-1;
+            }
+            tab[j+1]= key;
+        }
+        return tab;
+    }
 }
